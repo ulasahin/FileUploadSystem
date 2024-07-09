d5ea0b32f2eb536a3a2ffd4adb5061d7b23524b5
Canfer Ulaş Şahin <ulssahn@outlook.com>
2024-07-09 20:50:40 +0300

    Initial commit

---
+++ src/main/java/com/example/FileUploadSystem/FileUploadSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.FileUploadSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadSystemApplication.class, args);
+	}
+
+}
