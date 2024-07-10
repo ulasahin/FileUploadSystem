af5c830114757e14a42797673fa58179d612c21f
Canfer Ulaş Şahin <ulssahn@outlook.com>
2024-07-10 04:24:59 +0300

    x

--- src/main/java/com/example/FileUploadSystem/model/entities/User.java
+++ src/main/java/com/example/FileUploadSystem/model/entities/User.java
@@ -5,10 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
 import java.util.List;
 
 @Entity
--- src/main/java/com/example/FileUploadSystem/services/abstracts/UserService.java
+++ src/main/java/com/example/FileUploadSystem/services/abstracts/UserService.java
@@ -5,10 +5,6 @@ import com.example.FileUploadSystem.services.dtos.request.user.UpdateUserRequest
 import com.example.FileUploadSystem.services.dtos.response.user.AddUserReponse;
 import com.example.FileUploadSystem.services.dtos.response.user.DeleteUserResponse;
 import com.example.FileUploadSystem.services.dtos.response.user.UpdateUserResponse;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import java.util.List;
 
 public interface UserService  {
     AddUserReponse add(AddUserRequest request);
