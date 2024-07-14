4f776bdf86f8e3c8185f36ab908aab6aa3ecd4a5
Canfer Ulaş Şahin <ulssahn@outlook.com>
2024-07-14 19:54:27 +0300

    Edit FileController.java

--- src/main/java/com/example/FileUploadSystem/controller/FileController.java
+++ src/main/java/com/example/FileUploadSystem/controller/FileController.java
@@ -1,9 +1,11 @@
 package com.example.FileUploadSystem.controller;
 
 import com.example.FileUploadSystem.services.abstracts.FileService;
+import com.example.FileUploadSystem.services.dtos.request.file.UpdateFileRequest;
 import com.example.FileUploadSystem.services.dtos.response.file.AddFileResponse;
 import com.example.FileUploadSystem.services.dtos.response.file.DeleteFileResponse;
 import com.example.FileUploadSystem.services.dtos.response.file.GetFileResponse;
+import com.example.FileUploadSystem.services.dtos.response.file.UpdateFileResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -28,9 +30,25 @@ public class FileController {
     public List<GetFileResponse> getUserFiles(){
         return fileService.getUserFiles();
     }
+    @GetMapping("/getAll")
+    @ResponseStatus(HttpStatus.OK)
+    public List<GetFileResponse> getAll(){
+        return fileService.getAll();
+    }
+    @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public GetFileResponse getById(@RequestParam long id){
+        return fileService.getById(id);
+    }
     @DeleteMapping("/delete")
-    public DeleteFileResponse delete(long id){
+    public DeleteFileResponse delete(@RequestParam long id){
         return fileService.delete(id);
     }
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UpdateFileResponse update(@RequestBody UpdateFileRequest request){
+        return fileService.update(request);
+    }
+
 
 }
